d41961c95f3e5d6ff0b91fa11f182f187a3562e0
20danaai <dann343aa@gmail.com>
2026-03-23 18:03:47 +0300

    Create multiples_kyu6.py

---
+++ level1/multiples_kyu6.py
@@ -0,0 +1,9 @@
+import codewars_test as test
+def solution(number):
+    sum=0
+    if number<0:
+        return 0
+    for i in range(number):
+        if i%3==0 or i%5==0:
+            sum=sum+i
+    return sum
