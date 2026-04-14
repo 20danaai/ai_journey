8ef8509695a75640e8575a4ba36d1181a8193aad
20danaai <dann343aa@gmail.com>
2026-04-14 23:32:26 +0300

    Update Is_My_Friend_Cheating.py

--- level1/Is_My_Friend_Cheating.py
+++ level1/Is_My_Friend_Cheating.py
@@ -1,11 +1,11 @@
-#problem:find (a,b) from a sequence 1 to n such that a*b equals the sum of all other numbers in the sequence.
-#source:cdewars_kyu5
-#
+#Problem:find (a,b) from a sequence 1 to n such that a*b equals the sum of all other numbers in the sequence.
+#Source:cdewars_kyu5
+#Note:removed (a,b) and their product (*) is equal to the sum of the remaining num in the sequence
 def remov_nb(n):
     sum=n*(n+1)//2 # i use this not loop cause this faster (summing num manually)
     result=[]# storage
     for i in range(1,n+1):# n+1 for include n
-        b=(sum-i)/(i+1)   #problem says a*b=sum -a-b ,a*b+b=sum-a,b(a+1)=sum-a,b=(sum-a)/a+1
+        b=(sum-i)/(i+1)   #problem says a*b=sum -a-b look to the note you will understand ,a*b+b=sum-a,b(a+1)=sum-a,b=(sum-a)/a+1
         if b.is_integer()and b <=n and b!=i:
             result.append((i),int(b))) # include all the num
     return result
